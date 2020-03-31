aa5007070f120c4d5e989181560b5174ad162571
kalade99 <61167580+kalade99@users.noreply.github.com>
2020-03-31 20:24:23 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,29 @@ document.addEventListener('DOMContentLoaded', function() {
   // Your code here...
 
   // Create a User object with the following properties:
+
+  var user = {
+    firstName: 'Eimile',
+    lastName: 'Zalubaite',
+    age: 26,
+    occupation: 'CAD Designer',
+    friends: ['Lasse', 'Carol', 'Rasa', 'Eva'],
+    
+    address: {
+      number: 98,
+      street: 'Turnpike',
+      city: 'London',
+    },
+    hasCar: false,
+  
+
+
+
+    }
+
+
+
+  
   // - firstName = string
   // - lastName = string
   // - age = integer
@@ -9,19 +32,8 @@ document.addEventListener('DOMContentLoaded', function() {
   // - address = object, with number, street and city as properties
   // - friends = array
   // - hasCar = boolean
-  var user = {
-    firstName: 'fred',
-    lastName: 'Duffield',
-    age: 23,
-    occupation: 'Software Engineer',
-    address: {
-      number: 2,
-      street: 'Row 57',
-      city: 'London',
-    },
-    friends: ['Sarai', 'Harry', 'Jonny', 'Isabelle'],
-    hasCar: false
-  }
+  
+  
   // Using the existing HTML elements, assign each element its respective property from your new object
   // The first one has been done for you
 
@@ -29,30 +41,43 @@ document.addEventListener('DOMContentLoaded', function() {
   // name
   var nameElement = document.getElementById("name");
   nameElement.innerText = `${user.firstName} ${user.lastName}`;
-  
   // age
+  
   var ageElement = document.getElementById('age');
   ageElement.innerText = user.age;
   
+ 
+  
   // occupation
+  var occupationElement = document.getElementById("occupation");
+  occupationElement.innerText = `${user.occupation}`;
   
   // hasCar
+var hasCarElement = document.getElementById('hasCar');
+if (hasCar = true ) {
+hasCarElement.innerText = 'doest have a car';
+} else {
 
+hasCarElement.innerText = 'has a car'  ;
+}
   // INTERMEDIATE
   // address number
-  var numElement = document.getElementById('adrsNumber');
-  numElement.innerText = user.address.number
-  
-  // address street
-  
-  // address city
+  var addresNumber = document.getElementById("adrsNumber");
+  addresNumber.innerText = user.address.number;
 
+  // address street
+  var addressStreet = document.getElementById("adrsStreet");
+  addressStreet.innerText = user.address.street;
+  // address city
+var addressCity = document.getElementById("adrsCity");
+addressCity.innerText = user.address.city;
   // ADVANCED
   // loop through the friends and append each one onto the correct element
-  var friends = document.getElementById('friends');
-  for(let i = 0; i < user.friends.length; i++) {
-    const newListItem = document.createElement('li'); 
-    newListItem.innerText = user.friends[i]; 
-    friends.appendChild(newListItem);
-  }
+  var ul = document.getElementById("friends");
+for (var i = 0; i < user.friends.lenght; i++) {
+  var li = document.createElement('li');
+  li.innerText = user.friends[i];
+  friends.appendChild(il);
+}
+
 })
