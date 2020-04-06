d0bad5938344dd26d17c39eef18941c389f4086d
kalade99 <61167580+kalade99@users.noreply.github.com>
2020-04-06 20:59:40 +0200

    new

--- JS/theDom.js
+++ JS/theDom.js
@@ -14,14 +14,11 @@ img1.style.height = '80%';
 img1.style.transition = 'height 0.5s ease-in';
 
 
-})
-
-var aboutClick = document.getElementById('goToAbout');
-console.log(aboutClick);
-aboutClick.addEventListener('click', function() {
-aboutClick.style.color = ' #90ccf4';
 
 })
+
+
+
 /*activeImg.addEventListener('mouseout', function() {
 
     
@@ -30,6 +27,28 @@ aboutClick.style.color = ' #90ccf4';
     
     })*/
     
+    function changeColor(element, elements)
+{
+ 
+    for (var i = 0; i < elements.length; i++) {
+       if (elements[i] == element)
+       {
+           elements[i].style.color = "blue";
+       }
+       else
+       {
+           elements[i].style.color = "black";
+       }
+   };
+}
+
+var navBtns = document.getElementById('goToAbout');
+ 
+for (var i = 0; i < navBtns.length; i++)
+{
+    console.log(navBtns[i]);
+    navBtns[i].addEventListener("click", changeColor.bind(null, navBtns[i], navBtns));
+}
   
 
 });
